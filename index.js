6d3ccfc32eac57435f6c6f3b6bb221b6b86a602d
mohadmin25 <abuhamar008@gmail.com>
2022-08-20 16:20:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let multiply = 62
+let num1 = 2
+let num2 = 31
+num1 * num2;
 
-// Write your code here
+let random = 1
+
+let mod = 4
+let num3 = 20
+let num4 = 8
+num3 % num4
+
+let max = Math.max(20)
